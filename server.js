9d607406b8c8e46dc8c33b269e6bc98dcf7e5946
jakubtae <jakub.tomczyk.2005@gmail.com>
2023-05-08 20:12:46 +0200

    About me 1/3

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+app.use(express.static(path.join(__dirname, "public"))); //! static folder declaration
 
-app.get('/', (req, res) => {
-    res.send("Hello World!");
-})
+app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.json())
+
+const mainRouter = require("./routes/main");
+app.use("/", mainRouter);
+
+const plRouter = require("./routes/pl");
+app.use("/pl", plRouter);
 
 app.listen(3000, () => {
     console.log(`App is working I guess`);
