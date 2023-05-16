b2765ffd8733b7c2cc0cf70c02f5dc32d120e527
jakubtae <jakub.tomczyk.2005@gmail.com>
2023-05-16 19:30:49 +0200

    Whole but not projects

--- routes/main.js
+++ routes/main.js
@@ -2,13 +2,6 @@ const express = require('express');
 const { parse } = require('path');
 const router = express.Router();
 const fs = require('fs');
-const languages  = [];
-fs.readdirSync('./models/langs').forEach(function(file) {
-    var name = file.replace('.json', '');
-    languages.push(name);
-});
-
-console.log(languages);
 
 router.get('/', async (req, res) => {
     res.redirect("/en")
@@ -17,12 +10,33 @@ router.get('/', async (req, res) => {
 
 router.get('/:language', async (req, res) => {
     try{
-        console.log(req.params);
-        const wantedFile = req.params.language + ".json";
-        console.log(wantedFile);
-        res.render('main/landing.ejs',);
+        if(req.params != "favicon.ico"){
+            const filePath = "./models/langs/main/"+req.params.language + ".json";
+
+            if (!fs.existsSync(filePath)) return res.redirect("/en");
+
+            const language = JSON.parse(fs.readFileSync(filePath, "utf-8"))
+            
+            res.render('main/landing.ejs',language );
+        }
+    }catch(e){
+        res.send("error: " + e + " ").status(404);
+    }
+})
+
+router.get('/:language/projects/:pid', async (req,res)=>{
+    try{
+        if(req.params != "favicon.ico"){
+            const filePath = "./models/langs/projects/"+req.params.language + ".json";
+
+            if (!fs.existsSync(filePath)) return res.redirect("/en");
+
+            var language = JSON.parse(fs.readFileSync(filePath, "utf-8"))
+            language.pid = req.params.pid;
+            res.render('main/project.ejs', language);
+        }
     } catch(e){
-        res.send("error").status(404);
+        res.send("error: " + e + " ").status(404);
     }
 })
 
