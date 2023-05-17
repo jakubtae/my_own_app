d579d9fcba80ca7e2c88d70928b9c8051f183fad
jakubtae <jakub.tomczyk.2005@gmail.com>
2023-05-17 21:01:46 +0200

    Working lang?

--- routes/main.js
+++ routes/main.js
@@ -1,43 +1,56 @@
-const express = require('express');
-const { parse } = require('path');
+const express = require("express");
+const { parse } = require("path");
 const router = express.Router();
-const fs = require('fs');
+const fs = require("fs");
 
-router.get('/', async (req, res) => {
-    res.redirect("/pl")
-})
+router.get("/", async (req, res) => {
+  res.redirect("/pl");
+});
 
+router.get("/:language", checkLang,async (req, res) => {
+  try {
+    if (req.params != "favicon.ico") {
+      const filePath = "./models/langs/main/" + req.params.language + ".json";
 
-router.get('/:language',async (req, res) => {
-    try{
-        if(req.params != "favicon.ico"){
-            const filePath = "./models/langs/main/"+req.params.language + ".json";
+      if (!fs.existsSync(filePath)) return res.redirect("/en");
 
-            if (!fs.existsSync(filePath)) return res.redirect("/en");
+      const language = JSON.parse(fs.readFileSync(filePath, "utf-8"));
 
-            const language = JSON.parse(fs.readFileSync(filePath, "utf-8"))
-            
-            res.render('main/landing.ejs',language );
-        }
-    }catch(e){
-        res.send("error: " + e + " ").status(404);
+      res.render("main/landing.ejs", language);
     }
-})
+  } catch (e) {
+    res.send("error: " + e + " ").status(404);
+  }
+});
 
-router.get('/:language/projects/:pid', async (req,res)=>{
-    try{
-        if(req.params != "favicon.ico"){
-            const filePath = "./models/langs/projects/"+req.params.language + ".json";
+router.get("/:language/projects/:pid", checkLang, async (req, res) => {
+  try {
+    if (req.params != "favicon.ico") {
+      const filePath =
+        "./models/langs/projects/" + req.params.language + ".json";
 
-            if (!fs.existsSync(filePath)) return res.redirect("/en");
+      if (!fs.existsSync(filePath)) return res.redirect("/en");
 
-            var language = JSON.parse(fs.readFileSync(filePath, "utf-8"))
-            language.pid = req.params.pid;
-            res.render('main/project.ejs', language);
-        }
-    } catch(e){
-        res.send("error: " + e + " ").status(404);
+      var language = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+      language.pid = req.params.pid;
+      res.render("main/project.ejs", language);
     }
-})
+  } catch (e) {
+    res.send("error: " + e + " ").status(404);
+  }
+});
+
+async function checkLang(req, res, next) {
+  const link = "http://ip-api.com/json/" + req.ip;
+  const response = await fetch(link);
+  const data = await response.json();
+  if (data.countryCode !== "PL" && req.params.language === "pl") {
+    return res.redirect("/en"+req.originalUrl.slice(3));
+  }
+  if (data.countryCode === "PL" && req.params.language === "en") {
+    return res.redirect("/pl"+req.originalUrl.slice(3));
+  }
+  next();
+}
 
 module.exports = router;
