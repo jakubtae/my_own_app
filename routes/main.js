d2ce4f7be4b63556aae9a88d08158a6b4c36570f
jakubtae <jakub.tomczyk.2005@gmail.com>
2023-05-17 21:24:58 +0200

    Needs json with languages

--- routes/main.js
+++ routes/main.js
@@ -7,6 +7,39 @@ router.get("/", async (req, res) => {
   res.redirect("/pl");
 });
 
+router.get("/:language/force", async (req, res) => {
+    try {
+        if (req.params != "favicon.ico") {
+          const filePath = "./models/langs/main/" + req.params.language + ".json";
+    
+          if (!fs.existsSync(filePath)) return res.redirect("/en");
+    
+          const language = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+    
+          res.render("main/landing.ejs", language);
+        }
+      } catch (e) {
+        res.send("error: " + e + " ").status(404);
+      }
+})
+
+router.get("/:language/projects/:pid/force", async (req, res) => {
+    try {
+      if (req.params != "favicon.ico") {
+        const filePath =
+          "./models/langs/projects/" + req.params.language + ".json";
+  
+        if (!fs.existsSync(filePath)) return res.redirect("/en");
+  
+        var language = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+        language.pid = req.params.pid;
+        res.render("main/project.ejs", language);
+      }
+    } catch (e) {
+      res.send("error: " + e + " ").status(404);
+    }
+  });
+
 router.get("/:language", checkLang,async (req, res) => {
   try {
     if (req.params != "favicon.ico") {
@@ -22,6 +55,9 @@ router.get("/:language", checkLang,async (req, res) => {
     res.send("error: " + e + " ").status(404);
   }
 });
+
+
+
 
 router.get("/:language/projects/:pid", checkLang, async (req, res) => {
   try {
@@ -40,6 +76,8 @@ router.get("/:language/projects/:pid", checkLang, async (req, res) => {
   }
 });
 
+
+
 async function checkLang(req, res, next) {
   const link = "http://ip-api.com/json/" + req.ip;
   const response = await fetch(link);
@@ -47,7 +85,7 @@ async function checkLang(req, res, next) {
   if (data.countryCode !== "PL" && req.params.language === "pl") {
     return res.redirect("/en"+req.originalUrl.slice(3));
   }
-  if (data.countryCode === "PL" && req.params.language === "en") {
+  if (data.countryCode === "PL" && req.params.language === "en" || data.countryCode === "PL" && req.params.language === "pl") {
     return res.redirect("/pl"+req.originalUrl.slice(3));
   }
   next();
